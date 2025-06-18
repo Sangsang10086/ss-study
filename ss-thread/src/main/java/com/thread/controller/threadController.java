bc6532946660430472dd18eb65e62a0ff3adf390
sanghf <1059952597@qq.com>
2025-06-18 16:55:54 +0800

    2025_06_18_修改线程

--- ss-thread/src/main/java/com/thread/controller/threadController.java
+++ ss-thread/src/main/java/com/thread/controller/threadController.java
@@ -22,13 +22,13 @@ public class threadController {
     public Result<String> sendMsg(@RequestParam("email") String email, @RequestParam("data") String data){
         logger.info("注册成功邮件发送中...");
         //异步处理中间操作-手动处理第一个线程
-//        threadBiz.test1(email);
-//        threadBiz.test2(email);
-//        threadBiz.test3(email);
+        threadBiz.test1(email);
+        threadBiz.test2(email);
+        threadBiz.test3(email);
 
         // todo 对于需要所有线程执行完成之后的处理，可以采用如下方式
         //调用收集所有异步信息之后的处理逻辑
-//        threadBiz.test4(email);
+        threadBiz.test4(email);
         threadBiz.publishEvent(email,data);
         return Result.success("注册成功邮件发送中...",null);
     }
