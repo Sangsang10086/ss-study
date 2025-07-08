039f9c2b35a6e90ae397123e41b7838569ba6ac2
sanghf <1059952597@qq.com>
2025-07-08 10:35:52 +0800

    2025_07_08_添加webSocket模块

---
+++ ss-webSocket/src/main/java/com/websocket/SsWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsWebSocketApplication.class, args);
+    }
+
+}
