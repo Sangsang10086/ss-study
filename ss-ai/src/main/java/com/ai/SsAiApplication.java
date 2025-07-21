dca9443d6e9a07b37e2301a9f712b9ec31914d23
sanghf <1059952597@qq.com>
2025-07-21 11:10:29 +0800

    2025_07_21_创建ai模块

---
+++ ss-ai/src/main/java/com/ai/SsAiApplication.java
@@ -0,0 +1,13 @@
+package com.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsAiApplication.class, args);
+    }
+
+}
