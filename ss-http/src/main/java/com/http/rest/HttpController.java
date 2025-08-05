91b1c739801f372169dd16cabcacc6e210e48b0c
sanghf <1059952597@qq.com>
2025-08-05 16:06:03 +0800

    2025_08_05_封装ss-http模块

--- ss-http/src/main/java/com/http/rest/HttpController.java
+++ ss-http/src/main/java/com/http/rest/HttpController.java
@@ -1,25 +1,30 @@
 package com.http.rest;
 
-import com.common.rest.BaseController;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.logging.Logger;
 
 @RestController
-public class HttpController extends BaseController {
+@RequestMapping("/http")
+public class HttpController extends HttpServlet {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value = "/HelloHTTP", method = RequestMethod.GET)
     public void hello(HttpServletRequest request) {
         BufferedReader reader = null;
+        System.out.println(request);
         try {
             reader = request.getReader();
         } catch (IOException e) {
             Logger.getLogger("请求体解析失败");
         }
+        System.out.println(reader);
     }
 }
