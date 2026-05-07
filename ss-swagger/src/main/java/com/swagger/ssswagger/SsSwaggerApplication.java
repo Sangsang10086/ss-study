1bd2185c0564d5d2da98d544acd48d2fc7b6846b
sanghf <1059952597@qq.com>
2026-05-07 10:56:58 +0800

    20250814_添加ai

---
+++ ss-swagger/src/main/java/com/swagger/ssswagger/SsSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.swagger.ssswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsSwaggerApplication.class, args);
+    }
+
+}
---
+++ ss-swagger/src/test/java/com/swagger/ssswagger/SsSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swagger.ssswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
